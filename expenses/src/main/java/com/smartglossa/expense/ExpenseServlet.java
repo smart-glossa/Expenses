2e02cd26dd848d01f7463a41d3d415d821c38055
venkatachalamd <venkat@smartglossa.com>
2016-12-18 11:33:05 +0530

    get Expense detail by date

--- expenses/src/main/java/com/smartglossa/expense/ExpenseServlet.java
+++ expenses/src/main/java/com/smartglossa/expense/ExpenseServlet.java
@@ -3,6 +3,7 @@ package com.smartglossa.expense;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 import javax.servlet.ServletException;
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class ExpenseServlet extends HttpServlet {
@@ -25,7 +27,7 @@ public class ExpenseServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String op = request.getParameter("operation");
-		if(op.equals("add")){
+		if(op.equals("addExpense")){
 			JSONObject obj = new JSONObject();
 			String category = request.getParameter("category");
 			float amount = Float.parseFloat(request.getParameter("amnt"));
@@ -37,6 +39,25 @@ public class ExpenseServlet extends HttpServlet {
 				String query = "Insert into expense(category,amount,date) values('"+ category +"',"+ amount +",'"+ date +"')";
 				stmt.execute(query);
 				obj.put("status", "1");
+			} catch (Exception e) {
+				obj.put("status", "0");
+				e.printStackTrace();
+			}
+			response.getWriter().print(obj);
+		}else if(op.equals("getExpense")){
+			JSONObject obj = new JSONObject();
+			String date = request.getParameter("date");
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/expense", "root", "root");
+				Statement stmt = conn.createStatement();
+				String query = "select *from expense where date="+ date;
+				ResultSet rs = stmt.executeQuery(query);
+				if(rs.next()){
+					obj.put("category", rs.getString("category"));
+					obj.put("amount", rs.getInt("amount"));
+					
+				}
 			} catch (Exception e) {
 				obj.put("status", "0");
 				e.printStackTrace();
