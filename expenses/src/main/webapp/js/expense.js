778d7f56c6a6dc784b018a674d16be7ff835a865
venkatachalamd <venkat@smartglossa.com>
2016-12-18 17:29:51 +0530

    changes

--- expenses/src/main/webapp/js/expense.js
+++ expenses/src/main/webapp/js/expense.js
@@ -41,7 +41,7 @@ $(document).on('click','#add',function(){
 	})
 	
 }).on('click','#dropbtn',function(){
-	$('#dropdown').toggle();
+	$('#dropdown-content').toggle();
 }).on('keypress','#amt',function(e){
 	if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
         //display error message
